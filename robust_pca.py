cc8f3e01d1c034db982f8bdd65a7f257b0868035
felisevan <felisevan@protonmail.com>
2023-12-26 22:24:15 +0800

    remove explict CUDA transformation

--- robust_pca.py
+++ robust_pca.py
@@ -3,7 +3,7 @@ import torch
 
 class RPCA:
     def __init__(self, matrix, mu=None, lmd=None):
-        self.__M = torch.tensor(matrix, dtype=torch.float).cuda()
+        self.__M = torch.tensor(matrix, dtype=torch.float)
         self.L = torch.zeros_like(self.__M)
         self.S = torch.zeros_like(self.__M)
         self.__Y = torch.zeros_like(self.__M)
@@ -11,8 +11,8 @@ class RPCA:
             mu
             if mu
             else (
-                torch.prod(torch.tensor(self.__M.shape).cuda())
-                / (torch.tensor(4).cuda() * torch.norm(self.__M, p=1))
+                torch.prod(torch.tensor(self.__M.shape))
+                / (torch.tensor(4) * torch.norm(self.__M, p=1))
             )
         )
         # Cited from: Understanding Robust Principal Component Analysis (RPCA)
@@ -21,12 +21,9 @@ class RPCA:
         self.__lmd = (
             lmd
             if lmd
-            else (
-                torch.tensor(1).cuda()
-                / torch.sqrt(torch.max(torch.tensor(self.__M.shape).cuda()))
-            )
+            else (torch.tensor(1) / torch.sqrt(torch.max(torch.tensor(self.__M.shape))))
         )
-        self.__delta = torch.tensor(1e-7).cuda()
+        self.__delta = torch.tensor(1e-7)
         self.__lower_limit_value = self.__delta * torch.norm(self.__M, p=2)
         # Based on the original text
         # Link: https://arxiv.org/abs/0912.3599(Robust Principal Component Analysis?)
@@ -41,13 +38,13 @@ class RPCA:
     def shrinkage(self, matrix):
         return torch.sign(matrix) * torch.max(
             (torch.abs(matrix) - (self.__lmd * self.__mu)),
-            torch.zeros(matrix.shape).cuda(),
+            torch.zeros(matrix.shape),
         )
 
     def alternating_directions(self, max_iter_time=500):
         L2norm_value = torch.tensor(
             1
-        ).cuda()  # The initialized 1 is large enough for 'lowerlimitvalue' to enter the judgment
+        )  # The initialized 1 is large enough for 'lowerlimitvalue' to enter the judgment
         for _ in range(max_iter_time):
             if L2norm_value > self.__lower_limit_value:
                 self.L = self.singular_value_threshold(
