5ea4c8e1bdb5f51193e468d96f393634fd52d64c
14MBD4 <Salvationliewithin@protonmail.com>
2023-12-28 03:12:36 -0400

    Add test

--- test.py
+++ test.py
@@ -1,21 +1,22 @@
 import cv2
-from matplotlib import pyplot as plt
-
-import torch
 import numpy as np
+import torch
+from matplotlib import pyplot as plt
 
 import robust_pca
 
 if __name__ == "__main__":
     X = torch.from_numpy(
-        np.array(cv2.imread("out.jpg", cv2.IMREAD_GRAYSCALE), dtype=np.float64),
+        np.array(cv2.imread("Lena.bmp", cv2.IMREAD_GRAYSCALE), dtype=np.float64),
     ).to("cuda:0")
 
     plt.figure(1)
     plt.imshow(X.cpu(), cmap="gray")
     plt.show()
-    L, S = robust_pca.robust_pca(X, devices="cuda:0", max_iter_pass=50)
+    L, S = robust_pca.robust_pca(X, devices="cuda:0", max_iter_pass=500)
     plt.imshow(L.cpu(), cmap="gray")
     plt.show()
+    plt.savefig("L.png")
     plt.imshow(S.cpu(), cmap="gray")
     plt.show()
+    plt.savefig("S.png")
